410620011e090ec955184230df79914822a91602
alema <alema@10.0.0.114>
2023-04-08 14:59:38 -0400

    Inital commit

--- src/day2git/Day2HW3.java
+++ src/day2git/Day2HW3.java
@@ -6,5 +6,13 @@ public class Day2HW3 {
 		
 		System.out.println(" day2 HW3");
 	}
-
+public void add( int a, int b) {
+	System.out.println( a+b);
 }
+ public void add() {
+	 System.out.println("a+b");
+ }
+	
+}
+
+
